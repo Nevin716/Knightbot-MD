26bd68b2ff009181fe2840c8b04854e8baa44d20
Nevin716 <168355779+Nevin716@users.noreply.github.com>
2025-06-01 18:26:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "LIGHT",
   botOwner: 'Nevin', // Your name
-  ownerNumber: '6285147008494', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '917896304080', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
